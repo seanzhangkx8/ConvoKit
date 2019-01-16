41125f41291feca904f6efcde866f4aecf88ad5d
Jonathan Chang <jonathan.chang13@gmail.com>
2019-01-16 16:01:33 -0500

    Add Parser test case to test-new-corpus.py

--- examples/coordination/test-new-corpus.py
+++ examples/coordination/test-new-corpus.py
@@ -17,3 +17,12 @@ print("some utterances from a case:")
 for ut in list(corpus.iter_utterances())[:5]:
     if ut.meta["case"] == "02-1472":
         print(ut.text)
+
+print("testing Parser...")
+parser = convokit.Parser()
+corpus = parser.fit_transform(corpus)
+print("POS-tagging results:")
+for ut in list(corpus.iter_utterances())[:5]:
+    for token in ut.meta["parsed"]:
+        print("[%s %s]" % (token.tag_, token.text), end=' ')
+    print()
