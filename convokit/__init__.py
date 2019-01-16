6e911a07e5a04113d4d09e06af12dec51c5cd73e
Jonathan Chang <jonathan.chang13@gmail.com>
2019-01-16 15:23:02 -0500

    Add Transformer to module exports

--- convokit/__init__.py
+++ convokit/__init__.py
@@ -4,4 +4,5 @@ from .util import *
 from .questionTypology import *
 from .politenessStrategies import *
 from .hyperconvo import *
+from .transformer import *
 #__path__ = __import__('pkgutil').extend_path(__path__, __name__)
