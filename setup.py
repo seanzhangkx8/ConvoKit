72f23662325084f73940a0cfd69988ec24f2a6af
Caleb Chiam <calebchiam@gmail.com>
2019-05-06 19:22:09 -0400

    update version no.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email = "azw7@cornell.edu",
     url = "https://github.com/CornellNLP/Cornell-Conversational-Analysis-Toolkit",
     description = "Cornell Conversational Analysis Toolkit",
-    version = "2.0.0",
+    version = "2.0.1",
     packages = ["convokit", "convokit.politeness_api",
         "convokit.politeness_api.features"],
     package_data = {"convokit": ["data/*.txt"]},
