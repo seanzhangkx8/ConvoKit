6167b222ded0fd675da909352076a81c0f438e02
Caleb Chiam <calebchiam@gmail.com>
2019-05-22 22:04:54 -0400

    OrderedDict -> dict since dicts are ordered by default in py3.6

--- convokit/hyperconvo.py
+++ convokit/hyperconvo.py
@@ -2,7 +2,7 @@
 http://www.cs.cornell.edu/~cristian/Patterns_of_participant_interactions.html."""
 
 import itertools
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
 import numpy as np
 import scipy.stats
 from .transformer import Transformer
@@ -19,22 +19,22 @@ class Hypergraph:
     """
     def __init__(self):
         # public
-        self.nodes = OrderedDict()
-        self.hypernodes = OrderedDict()
+        self.nodes = dict()
+        self.hypernodes = dict()
 
         # private
-        self.adj_out = OrderedDict()  # out edges for each (hyper)node
-        self.adj_in = OrderedDict()   # in edges for each (hyper)node
+        self.adj_out = dict()  # out edges for each (hyper)node
+        self.adj_in = dict()   # in edges for each (hyper)node
 
     def add_node(self, u: Hashable, info: Optional[Dict]=None) -> None:
         self.nodes[u] = info if info is not None else dict()
-        self.adj_out[u] = OrderedDict()
-        self.adj_in[u] = OrderedDict()
+        self.adj_out[u] = dict()
+        self.adj_in[u] = dict()
 
     def add_hypernode(self, name: Hashable, nodes: Collection[Hashable], info=None) -> None:
         self.hypernodes[name] = set(nodes)
-        self.adj_out[name] = OrderedDict()
-        self.adj_in[name] = OrderedDict()
+        self.adj_out[name] = dict()
+        self.adj_in[name] = dict()
 
     # edge or hyperedge
     def add_edge(self, u: Hashable, v: Hashable, info=None) -> None:
@@ -51,27 +51,27 @@ class Hypergraph:
         self.adj_in[v][u].append(info)
 
     def edges(self) -> Dict[Tuple[Hashable, Hashable], List]:
-        return OrderedDict(((u, v), lst) for u, d in self.adj_out.items()
+        return dict(((u, v), lst) for u, d in self.adj_out.items()
                            for v, lst in d.items())
 
     def outgoing_nodes(self, u: Hashable) -> Dict[Hashable, List]:
         assert u in self.adj_out
-        return OrderedDict((v, lst) for v, lst in self.adj_out[u].items()
+        return dict((v, lst) for v, lst in self.adj_out[u].items()
                            if v in self.nodes)
 
     def outgoing_hypernodes(self, u) -> Dict[Hashable, List]:
         assert u in self.adj_out
-        return OrderedDict((v, lst) for v, lst in self.adj_out[u].items()
+        return dict((v, lst) for v, lst in self.adj_out[u].items()
                            if v in self.hypernodes)
 
     def incoming_nodes(self, v: Hashable) -> Dict[Hashable, List]:
         assert v in self.adj_in
-        return OrderedDict((u, lst) for u, lst in self.adj_in[v].items() if u in
+        return dict((u, lst) for u, lst in self.adj_in[v].items() if u in
                            self.nodes)
 
     def incoming_hypernodes(self, v: Hashable) -> Dict[Hashable, List]:
         assert v in self.adj_in
-        return OrderedDict((u, lst) for u, lst in self.adj_in[v].items() if u in
+        return dict((u, lst) for u, lst in self.adj_in[v].items() if u in
                            self.hypernodes)
 
     def outdegrees(self, from_hyper: bool=False, to_hyper: bool=False) -> List[int]:
@@ -224,7 +224,7 @@ class HyperConvo(Transformer):
             uts = {utt.id: utt for utt in corpus.iter_utterances()}
 
         G = Hypergraph()
-        username_to_utt_ids = OrderedDict()
+        username_to_utt_ids = dict()
         reply_edges = []
         speaker_to_reply_tos = defaultdict(list)
         speaker_target_pairs = set()
