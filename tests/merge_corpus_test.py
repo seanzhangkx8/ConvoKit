9d1ecee0754fbf03072a561ebc7703a5c0bc07d2
calebchiam <calebchiam@gmail.com>
2019-08-17 15:01:45 +0800

    Update merge_corpus_test.py

--- tests/merge_corpus_test.py
+++ tests/merge_corpus_test.py
@@ -1,10 +1,5 @@
 import unittest
-
-import os
-os.chdir('..')
-from convokit import Utterance, User, Corpus
-os.chdir('./tests')
-
+from convokit.model import Utterance, User, Corpus
 
 class CorpusMerge(unittest.TestCase):
     def test_no_overlap(self):
