305704fcc2f3ab3fcb0cb953d1514b20c261cfec
calebchiam <calebchiam@gmail.com>
2019-08-22 01:04:10 +0800

    configuring scripts to sync with zissou automatically

--- doc/publish.sh
+++ doc/publish.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 make html
 rsync -r build/html/ $ZISSOU_USER@zissou.infosci.cornell.edu:/var/www/html/convokit/documentation
-
-# publish to my personal site
-#rsync -a build/html/ ~/dev/qema.github.io/convokit/
-#pushd .
-#cd ~/dev/qema.github.io
-#nanosite p
-#popd
--- website/update-site.sh
+++ website/update-site.sh
@@ -2,3 +2,5 @@
 # copy readme but remove title
 tail -n +2 ../README.md > index.md+
 nanosite build
+rsync output/index.html $ZISSOU_USER@zissou.infosci.cornell.edu:/var/www/html/socialkit/
+rsync output/style.css $ZISSOU_USER@zissou.infosci.cornell.edu:/var/www/html/socialkit/
