4da304e95c92c3ea8dbcbf12a0184c451ed2ff90
calebchiam <calebchiam@gmail.com>
2020-02-23 00:39:56 -0500

    refreshing numbers for release

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="convokit",
@@ -13,32 +13,35 @@ setup(
                 "convokit.coordination",
                 "convokit.fighting_words",
                 "convokit.forecaster",
+                "convokit.forecaster.CRAFT",
                 "convokit.hyperconvo",
                 "convokit.model",
                 "convokit.paired_prediction",
+                "convokit.phrasing_motifs",
                 "convokit.politeness_api",
                 "convokit.politeness_api.features",
                 "convokit.politenessStrategies",
-                "convokit.phrasing_motifs",
                 "convokit.prompt_types",
                 "convokit.ranker",
                 "convokit.text_processing",
-                "convokit.userConvoDiversity",
                 "convokit.user_convo_helpers",
+                "convokit.userConvoDiversity",
               ],
     package_data={"convokit": ["data/*.txt"]},
     install_requires=[
         "matplotlib>=3.0.0",
         "pandas>=0.23.4",
-        "msgpack-numpy==0.4.3.2",
-        "spacy==2.0.12",
+        "msgpack-numpy>=0.4.3.2",
+        "spacy>=2.0.12",
         "scipy>=1.1.0",
         "scikit-learn>=0.20.0",
         "nltk>=3.4",
-        "dill==0.2.9"
+        "dill>=0.2.9",
+        "joblib>=0.13.2",
+        "clean-text>=0.1.1",
+        "unidecode>=1.1.1"
     ],
     extras_require={
         'neuralnetwork': ["torch>=0.12"],
-        'cleantext': ["clean-text>=0.1.1"]
     }
 )
