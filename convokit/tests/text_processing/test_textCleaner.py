945760a7ee5f39c4539c48b002cc9171d11d069b
mwilbz <mattwilber94@gmail.com>
2020-06-03 17:08:14 -0700

    Add tests for textCleaner.py

---
+++ convokit/tests/text_processing/test_textCleaner.py
@@ -0,0 +1,34 @@
+from copy import deepcopy
+import unittest
+
+from convokit.tests.util import burr_sir_corpus
+from convokit.text_processing.textCleaner import TextCleaner
+
+
+class TestTextCleaner(unittest.TestCase):
+    def test_no_options(self):
+        corpus = burr_sir_corpus()
+        cleaner = TextCleaner(text_cleaner=lambda text: 'cleaned text')
+        cleaned_corpus = cleaner.transform(deepcopy(corpus))
+
+        for utterance in cleaned_corpus.iter_utterances():
+            self.assertEqual(utterance.text, 'cleaned text')
+
+    def test_dont_replace_text(self):
+        corpus = burr_sir_corpus()
+        cleaner = TextCleaner(text_cleaner=lambda text: 'cleaned text', replace_text=False)
+        cleaned_corpus = cleaner.transform(deepcopy(corpus))
+
+        for original_utterance, cleaned_utterance in zip(corpus.iter_utterances(), cleaned_corpus.iter_utterances()):
+            self.assertEqual(original_utterance.text, cleaned_utterance.text)
+            self.assertEqual(cleaned_utterance.meta['cleaned'], 'cleaned text')
+
+
+    def test_save_original(self):
+        corpus = burr_sir_corpus()
+        cleaner = TextCleaner(text_cleaner=lambda text: 'cleaned text', replace_text=True, save_original=True)
+        cleaned_corpus = cleaner.transform(deepcopy(corpus))
+
+        for original_utterance, cleaned_utterance in zip(corpus.iter_utterances(), cleaned_corpus.iter_utterances()):
+            self.assertEqual(cleaned_utterance.text, 'cleaned text')
+            self.assertEqual(original_utterance.text, cleaned_utterance.meta['original'])
