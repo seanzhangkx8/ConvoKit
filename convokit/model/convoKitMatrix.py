56c0fcebcae4d405aad59a5bf9e72937cd4d5be0
calebchiam <calebchiam@gmail.com>
2020-08-03 19:37:38 +0800

    added vstack and hstack functionality

--- convokit/model/convoKitMatrix.py
+++ convokit/model/convoKitMatrix.py
@@ -4,7 +4,7 @@ import pickle
 import os
 import numpy as np
 from convokit.util import warn
-from scipy import sparse
+from scipy.sparse import issparse, csr_matrix, hstack, vstack
 
 class ConvoKitMatrix:
     """
@@ -64,11 +64,11 @@ class ConvoKitMatrix:
             if not as_dataframe:
                 return self.matrix[indices]
             else:
-                matrix = self.matrix.toarray() if sparse.issparse(self.matrix) else self.matrix
+                matrix = self.matrix.toarray() if issparse(self.matrix) else self.matrix
                 return pd.DataFrame(matrix[indices], index=ids, columns=self.columns)
         else:
             col_indices = [self.cols_to_idx[col] for col in columns]
-            matrix = self.matrix.toarray() if sparse.issparse(self.matrix) else self.matrix
+            matrix = self.matrix.toarray() if issparse(self.matrix) else self.matrix
             submatrix = matrix[indices, col_indices].reshape(len(indices), len(col_indices))
             if as_dataframe:
                 return pd.DataFrame(submatrix, index=ids, columns=columns)
@@ -85,7 +85,7 @@ class ConvoKitMatrix:
             d[id] = {self.columns[i]: v for i, v in enumerate(row)}
         return d
 
-    def to_dataframe(self):
+    def to_dataframe(self) -> pd.DataFrame:
         """
         Converts the matrix of vectors into a pandas DataFrame.
 
@@ -93,7 +93,7 @@ class ConvoKitMatrix:
         """
         index = {idx: id_ for id_, idx in self.ids_to_idx.items()}
         sorted_ids = [index[idx] for idx in sorted(index)]
-        matrix = self.matrix.toarray() if sparse.issparse(self.matrix) else self.matrix
+        matrix = self.matrix.toarray() if issparse(self.matrix) else self.matrix
         return pd.DataFrame(matrix, index=sorted_ids, columns=self.columns)
 
     @staticmethod
@@ -130,15 +130,73 @@ class ConvoKitMatrix:
         :param dirpath: directory path to Corpus
         :return: None
         """
-        if not sparse.issparse(self.matrix):
+        if not issparse(self.matrix):
             temp = self.matrix
-            self.matrix = sparse.csr_matrix(self.matrix)
+            self.matrix = csr_matrix(self.matrix)
             with open(os.path.join(dirpath, 'vectors.{}.p'.format(self.name)), 'wb') as f:
                 pickle.dump(self, f)
             self.matrix = temp
         else:
             with open(os.path.join(dirpath, 'vectors.{}.p'.format(self.name)), 'wb') as f:
                 pickle.dump(self, f)
+
+    def subset(self, ids=None, columns=None):
+        """
+        Get a (subset) copy of the ConvoKitMatrix object according to specified subset of ids and columns
+        :param ids: list of ids to be included in the subset; all by default
+        :param columns: list of columns to be included in the subset; all by default
+        :return: a new ConvoKitMatrix object with the subset of
+        """
+        ids = ids if ids is not None else self.ids
+        columns = columns if columns is not None else self.columns
+
+        submatrix = self.to_dataframe().loc[ids][columns]
+        return ConvoKitMatrix(name=self.name,
+                              matrix=csr_matrix(submatrix.values.astype('float64')),
+                              ids=ids,
+                              columns=columns)
+
+    @staticmethod
+    def hstack(name, matrices: List['ConvoKitMatrix']):
+        """
+        Combines multiple ConvoKitMatrices into a single ConvoKitMatrix by stacking them horizontally (i.e. each
+        constituent matrix must have the same ids).
+
+        :param name: name of new matrix
+        :param matrices: constituent ConvoKiMatrices
+        :return: a new ConvoKitMatrix
+        """
+        assert len(matrices) > 1
+        stacked = hstack([csr_matrix(m.matrix) for m in matrices])
+        columns = []
+        for m in matrices:
+            columns.extend(m.columns)
+
+        return ConvoKitMatrix(name=name,
+                              matrix=stacked,
+                              ids=matrices[0].ids,
+                              columns=columns)
+
+    @staticmethod
+    def vstack(name: str, matrices: List['ConvoKitMatrix']):
+        """
+        Combines multiple ConvoKitMatrices into a single ConvoKitMatrix by stacking them horizontally (i.e. each
+        constituent matrix must have the same columns).
+
+        :param name: name of new matrix
+        :param matrices: constituent ConvoKiMatrices
+        :return: a new ConvoKitMatrix
+        """
+        assert len(matrices) > 1
+        stacked = vstack([csr_matrix(m.matrix) for m in matrices])
+        ids = []
+        for m in matrices:
+            ids.extend(list(m.ids))
+
+        return ConvoKitMatrix(name=name,
+                              matrix=stacked,
+                              ids=ids,
+                              columns=matrices[0].columns)
 
     def __repr__(self):
         return "ConvoKitMatrix('name': {}, 'matrix': {})".format(self.name, repr(self.matrix))
