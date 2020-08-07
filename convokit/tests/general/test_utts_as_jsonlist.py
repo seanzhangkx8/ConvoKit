fcdee301b444be249d360a91e6eea8b59633f747
calebchiam <calebchiam@gmail.com>
2020-08-07 19:44:38 +0800

    restructuring tests directory + existing corpora load test

--- convokit/tests/general/test_utts_as_jsonlist.py
+++ convokit/tests/general/test_utts_as_jsonlist.py
@@ -137,4 +137,28 @@ class CorpusMerge(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+
+    speaker_byte_arr1 = bytearray([120, 3, 255, 0, 100])
+    speaker_byte_arr2 = bytearray([110, 3, 255, 90])
+    utt_byte_arr1 = bytearray([99, 44, 33])
+    utt_byte_arr2 = bytearray([110, 200, 220, 28])
+
+    corpus1 = Corpus(utterances = [
+        Utterance(id="0", text="hello world",
+                  speaker=Speaker(id="alice", meta={'speaker_binary_data': speaker_byte_arr1, 'index': 99}),
+                  meta={'utt_binary_data': utt_byte_arr1}),
+
+        Utterance(id="1", text="my name is bob",
+                  speaker=Speaker(id="bob", meta={'speaker_binary_data': speaker_byte_arr2}),
+                  meta={'utt_binary_data': utt_byte_arr2}),
+        Utterance(id="2", text="this is a test", speaker=Speaker(id="charlie")),
+    ])
+
+    print(corpus1.meta_index.utterances_index)
+
+    corpus1.get_utterance("0").meta['utt_binary_data'] = 5
+
+    print(corpus1.meta_index.utterances_index)
+
+    corpus1.print_summary_stats()
