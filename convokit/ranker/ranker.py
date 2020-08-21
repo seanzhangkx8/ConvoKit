2c8efad93a148b99d55268b6fe627225a111cb33
calebchiam <calebchiam@gmail.com>
2020-08-21 15:56:37 +0800

    added customizable attribute names

--- convokit/ranker/ranker.py
+++ convokit/ranker/ranker.py
@@ -1,7 +1,9 @@
-from convokit.model import Corpus
 from typing import List, Callable, Union
-from convokit import Transformer, CorpusComponent
 import pandas as pd
+
+from convokit import Corpus, Transformer, CorpusComponent
+from convokit.util import deprecation
+
 
 class Ranker(Transformer):
     """
@@ -9,16 +11,25 @@ class Ranker(Transformer):
 
     :param obj_type: type of Corpus object to rank: 'conversation', 'speaker', or 'utterance'
     :param score_func: function for computing the score of a given object
-    :param score_feat_name: metadata feature name to use in annotation for score value, default: "score"
-    :param rank_feat_name: metadata feature name to use in annotation for the rank value, default: "[score_feat_name]_rank"
+    :param score_attribute_name: metadata attribute name to use in annotation for score value, default: "score"
+    :param rank_attribute_name: metadata attribute name to use in annotation for the rank value, default: "rank"
     """
     def __init__(self, obj_type: str,
                  score_func: Callable[[CorpusComponent], Union[int, float]],
-                 score_feat_name: str = "score", rank_feat_name: str = None):
+                 score_attribute_name: str = "score",
+                 score_feat_name=None,
+                 rank_attribute_name: str = "rank",
+                 rank_feat_name=None):
         self.obj_type = obj_type
         self.score_func = score_func
-        self.score_feat_name = score_feat_name
-        self.rank_feat_name = score_feat_name + "_rank" if rank_feat_name is None else rank_feat_name
+        self.score_attribute_name = score_attribute_name if score_feat_name is None else score_feat_name
+        self.rank_attribute_name = rank_attribute_name if rank_feat_name is None else rank_feat_name
+
+        if score_feat_name is not None:
+            deprecation("Ranker's score_feat_name parameter", 'score_attribute_name')
+
+        if rank_feat_name is not None:
+            deprecation("Ranker's rank_feat_name parameter", 'rank_attribute_name')
 
     def transform(self, corpus: Corpus, y=None, selector: Callable[[CorpusComponent], bool] = lambda obj: True) -> Corpus:
         """
@@ -32,17 +43,17 @@ class Ranker(Transformer):
                      "speaker": corpus.iter_speakers,
                      "utterance": corpus.iter_utterances}
         obj_scores = [(obj.id, self.score_func(obj)) for obj in obj_iters[self.obj_type](selector)]
-        df = pd.DataFrame(obj_scores, columns=["id", self.score_feat_name]) \
-            .set_index('id').sort_values(self.score_feat_name, ascending=False)
-        df[self.rank_feat_name] = [idx+1 for idx, _ in enumerate(df.index)]
+        df = pd.DataFrame(obj_scores, columns=["id", self.score_attribute_name]) \
+            .set_index('id').sort_values(self.score_attribute_name, ascending=False)
+        df[self.rank_attribute_name] = [idx + 1 for idx, _ in enumerate(df.index)]
 
         for obj in corpus.iter_objs(obj_type=self.obj_type):
             if obj.id in df.index:
-                obj.add_meta(self.score_feat_name, df.loc[obj.id][self.score_feat_name])
-                obj.add_meta(self.rank_feat_name, df.loc[obj.id][self.rank_feat_name])
+                obj.add_meta(self.score_attribute_name, df.loc[obj.id][self.score_attribute_name])
+                obj.add_meta(self.rank_attribute_name, df.loc[obj.id][self.rank_attribute_name])
             else:
-                obj.add_meta(self.score_feat_name, None)
-                obj.add_meta(self.rank_feat_name, None)
+                obj.add_meta(self.score_attribute_name, None)
+                obj.add_meta(self.rank_attribute_name, None)
         return corpus
 
     def transform_objs(self, objs: List[CorpusComponent]):
@@ -53,12 +64,12 @@ class Ranker(Transformer):
         :return: list of annotated COrpus objects
         """
         obj_scores = [(obj.id, self.score_func(obj)) for obj in objs]
-        df = pd.DataFrame(obj_scores, columns=["id", self.score_feat_name]) \
-            .set_index('id').sort_values(self.score_feat_name, ascending=False)
-        df[self.rank_feat_name] = [idx+1 for idx, _ in enumerate(df.index)]
+        df = pd.DataFrame(obj_scores, columns=["id", self.score_attribute_name]) \
+            .set_index('id').sort_values(self.score_attribute_name, ascending=False)
+        df[self.rank_attribute_name] = [idx + 1 for idx, _ in enumerate(df.index)]
         for obj in objs:
-            obj.add_meta(self.score_feat_name, df.loc[obj.id][self.score_feat_name])
-            obj.add_meta(self.rank_feat_name, df.loc[obj.id][self.rank_feat_name])
+            obj.add_meta(self.score_attribute_name, df.loc[obj.id][self.score_attribute_name])
+            obj.add_meta(self.rank_attribute_name, df.loc[obj.id][self.rank_attribute_name])
         return objs
 
     def summarize(self, corpus: Corpus, selector: Callable[[CorpusComponent], bool] = lambda obj: True):
@@ -72,11 +83,11 @@ class Ranker(Transformer):
         obj_iters = {"conversation": corpus.iter_conversations,
                      "speaker": corpus.iter_speakers,
                      "utterance": corpus.iter_utterances}
-        obj_scores_ranks = [(obj.id, obj.meta[self.score_feat_name], obj.meta[self.rank_feat_name])
+        obj_scores_ranks = [(obj.id, obj.meta[self.score_attribute_name], obj.meta[self.rank_attribute_name])
                       for obj in obj_iters[self.obj_type](selector)]
 
-        df = pd.DataFrame(obj_scores_ranks, columns=["id", self.score_feat_name, self.rank_feat_name])\
-                        .set_index('id').sort_values(self.rank_feat_name, ascending=True)
+        df = pd.DataFrame(obj_scores_ranks, columns=["id", self.score_attribute_name, self.rank_attribute_name])\
+                        .set_index('id').sort_values(self.rank_attribute_name, ascending=True)
 
         return df
 
@@ -87,8 +98,8 @@ class Ranker(Transformer):
         :param objs: list of annotated corpus objects
         :return: a pandas DataFrame
         """
-        obj_scores_ranks = [(obj.id, obj.meta[self.score_feat_name], obj.meta[self.rank_feat_name]) for obj in objs]
-        df = pd.DataFrame(obj_scores_ranks, columns=["id", self.score_feat_name, self.rank_feat_name]) \
-            .set_index('id').sort_values(self.rank_feat_name, ascending=True)
+        obj_scores_ranks = [(obj.id, obj.meta[self.score_attribute_name], obj.meta[self.rank_attribute_name]) for obj in objs]
+        df = pd.DataFrame(obj_scores_ranks, columns=["id", self.score_attribute_name, self.rank_attribute_name]) \
+            .set_index('id').sort_values(self.rank_attribute_name, ascending=True)
 
         return df
