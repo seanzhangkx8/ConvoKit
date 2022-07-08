55484d4435f25a1a6c75a2378b52c07ea65441a0
calebchiam <calebchiam@gmail.com>
2022-07-08 11:04:30 +0300

    full list of autodoc mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -354,4 +354,14 @@ texinfo_documents = [
 # texinfo_no_detailmenu = False
 autodoc_dumb_docstring = True
 
-autodoc_mock_imports = ["pandas"]
+autodoc_mock_imports = [
+    "pandas",
+    "tqdm",
+    "matplotlib",
+    "spacy",
+    "scipy",
+    "nltk",
+    "clean-text",
+    "unidecode",
+    "scikit-learn",
+]
