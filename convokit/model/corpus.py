c4398b2821a972a8aa6ab4bea7e4fd4d592abfa7
Oscar <os2304@columbia.edu>
2022-07-19 14:25:22 +0800

    boilerplate template for adding speaker and utterances individually

--- convokit/model/corpus.py
+++ convokit/model/corpus.py
@@ -180,6 +180,10 @@ class Corpus:
                 speaker.owner = self
             for _, utt in self.utterances.items():
                 utt.owner = self
+                
+        else: #initialize empty Corpus 
+            self.utterances = dict()
+            self.speakers = dict()
 
         if merge_lines:
             self.utterances = merge_utterance_lines(self.utterances)
@@ -190,6 +194,43 @@ class Corpus:
         self.meta_index.enable_type_check()
         self.update_speakers_data()
 
+    def create_speaker(
+        self,
+        owner=None,
+        id: str = None,
+        name: str = None,
+        utts=None,
+        convos=None,
+        meta: Optional[Dict] = None,
+    ):
+        speaker = Speaker(owner=owner,id=id,name=name,utts=utts,)
+        self.speakers[speaker.id] = speaker
+        speaker.owner = self
+        # self.conversations = initialize_conversations(self, self.utterances, convos_data)
+        self.meta_index.enable_type_check()
+        self.update_speakers_data()
+    
+    def create_utterance(
+        self,
+        owner=None,
+        id: Optional[str] = None,
+        speaker: Optional[Speaker] = None,
+        user: Optional[Speaker] = None,
+        conversation_id: Optional[str] = None,
+        root: Optional[str] = None,
+        reply_to: Optional[str] = None,
+        timestamp: Optional[int] = None,
+        text: str = "",
+        meta: Optional[Dict] = None,
+    ):
+        utt = Utterance(owner=owner,id=id,speaker=speaker,user=user,conversation_id=conversation_id,root=root,reply_to=reply_to,timestamp=timestamp,text=text,meta=meta)
+        self.utterances[utt.id] = utt
+        utt.owner = self
+        # self.conversations = initialize_conversations(self, self.utterances, convos_data)
+        self.meta_index.enable_type_check()
+        self.update_speakers_data()
+
+        
     @property
     def vectors(self):
         return self.meta_index.vectors
