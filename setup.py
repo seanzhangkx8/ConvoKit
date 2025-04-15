2442e292f9f0ebaeee2cff1851f8e05b57f1831b
seanzhangkx8 <106214464+seanzhangkx8@users.noreply.github.com>
2025-04-15 00:22:59 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -70,8 +70,8 @@ setup(
         "bitsandbytes",
         "transformers",
         "trl>=0.12.2",
-        # "tensorflow>=2.18.0",
-        # "tf-keras>=2.17.0,<3.0.0",
+        "tensorflow>=2.18.0",
+        "tf-keras>=2.17.0,<3.0.0",
     ],
     extras_require={
         "craft": ["torch>=0.12"],
